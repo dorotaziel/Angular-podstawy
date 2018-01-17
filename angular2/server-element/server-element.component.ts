49b72181cb81bfe29a468d4b31da5d92832b5119
dorotaziel <35374887+dorotaziel@users.noreply.github.com>
2018-01-17 18:10:28 +0100

    Enkapsulacja css.

--- angular2/server-element/server-element.component.ts
+++ angular2/server-element/server-element.component.ts
@@ -1,9 +1,13 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input,  ViewEncapsulation } from '@angular/core';
+
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
-  styleUrls: ['./server-element.component.css']
+  styleUrls: ['./server-element.component.css'],
+  encapsulation: ViewEncapsulation.None // - wyłączenie enkapsulacji dla konkretnego componentu.
+                                        // Native - używa technologii shadow DOM, 
+                                        // Używa domyślnej technologii angulara, gdzie każdy każdy component korzysta z własnego css
 })
 export class ServerElementComponent implements OnInit {
   @Input('srvElement') element: {type: string, name: string, content: string};
